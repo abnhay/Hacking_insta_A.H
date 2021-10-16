29b6fe5f3226d32c616a59214e65423006163a28
ابن الحي <75506961+abnhay@users.noreply.github.com>
2021-10-16 11:12:53 +0300

    Add files via upload

---
+++ lib.sh
@@ -0,0 +1,15 @@
+apt update -y && apt upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pip install sys
+pip install requests
+pip install uuid
+pip install webbrowser
+pip install socket
+pip install random
+pip install secrets
+pip install pyfiglet
+clear
+chmod +x .insta_hack.py
+python .insta_hack.py
